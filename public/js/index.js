d70da9e2a461c99940ec686e5601d80306983b93
jAlfredsson <john@killnoise.com>
2018-09-24 17:46:43 +0200

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', () => {
   console.log('connected to server');
-
-socket.emit('createMessage', {
-    from: 'John',
-    text: 'trafad'
-})
 })
 
 socket.on('disconnect', () => {
